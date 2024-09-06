1adf4c34c8a7c2403796cffb6f94c2149928bd9c
23DP3RObru <166131441+23DP3RObru@users.noreply.github.com>
2024-09-06 11:20:11 +0000

    06.09.2024 Darbs

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,31 @@
 package lv.rvt;
-
+import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
     {
         // Make your first Hello World app!
+        
+        // Komentārs
+        // VSCode keybind: Ctrl +
+        
+        // String name = "John";
+        // int age = 20;
+        // boolean isOnline = false;
+        // System.out.println(name);
+        // System.out.println(age);
+        // System.out.println(isOnline);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ievadi savu vārdu: ");
+        String vards = scanner.nextLine();
+        System.out.println("Sveiks " + vards + "!");
+        System.out.println("Ievadi savu uzvārdu: ");
+        String uzvards = scanner.nextLine();
+        System.out.println("Ievadi savu grupu: ");
+        String grupa = scanner.nextLine();
+        System.out.println("Students: " + vards +" "+ uzvards +", Grupa: "+ grupa);
+
 
     }
 
---
+++ src/main/resources/script.py
@@ -0,0 +1,3 @@
+name="John"
+age=1
+isOnline= True
