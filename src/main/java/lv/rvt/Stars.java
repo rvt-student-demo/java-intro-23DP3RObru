334dbf246838689c392e340a7df59c5a37987e6e
23DP3RObru <166131441+23DP3RObru@users.noreply.github.com>
2024-10-11 05:59:08 +0000

    Stars part 1

--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -1,7 +1,21 @@
 package lv.rvt;
 import java.util.*;
 public class Stars{
-    public static void printStars(){
-        System.out.println("*");
+    public static void printStars(int number){
+        int times = 0;
+        String star = "*";
+        while (times < number) {
+            System.out.print(star);
+            times += 1;
+            
+        }
+        System.out.println("");
+        
+    }
+
+    public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
     }
 }
