bf03a4bf32e87b90b3368f8e8066acd850a0740f
23DP3RObru <166131441+23DP3RObru@users.noreply.github.com>
2024-10-30 08:43:22 +0000

    arrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,26 +1,38 @@
 package lv.rvt;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        ArrayList<Integer> numbers = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter numbers, -1 to end: ");
-        while (true) {
-            
-            String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("-1")) {
-                break;
-            }
-            numbers.add(Integer.parseInt(input));
-            
-
+        Random random = new Random();
+        int[] array = new int[10];
+        for(int i=0; i<10; i++){
+            array[i] = random.nextInt(10);
         }
-        System.out.println(numbers);        }
+        System.out.println(Arrays.toString(array));
+        int max = array[0];
+        int min = array[0];
+        
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println(min);
+        System.out.println(max);
+
         
     }
+
+}
     
     
 
