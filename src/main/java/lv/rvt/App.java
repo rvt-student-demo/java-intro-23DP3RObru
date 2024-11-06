0133472150e8d8030318f61d5a6b3e5e66a745af
23DP3RObru <166131441+23DP3RObru@users.noreply.github.com>
2024-11-06 07:43:39 +0000

    First bank transfer

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,15 +9,15 @@ public class App
     public static void main( String[] args )
     {
         Account newaccount = new Account("New account", 0.00);
-Account matthewaccount = new Account("Matthews account", 100.00);
+Account matthewaccount = new Account("Matthews account", 1000.00);
 
 System.out.println("Initial state");
 System.out.println(newaccount);
 System.out.println(matthewaccount);
 
-matthewaccount.withdraw(20);
+matthewaccount.withdraw(100);
 System.out.println("The balance of Matthews account is now: " + matthewaccount.balance());
-newaccount.deposit(20);
+newaccount.deposit(100);
 System.out.println("The balance of the new account is now: " + newaccount.balance());
 
 System.out.println("End state");
