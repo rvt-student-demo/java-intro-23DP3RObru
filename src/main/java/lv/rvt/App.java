f6e38be0ce1b4afbb3bd283ff886d204ae96cc47
23DP3RObru <166131441+23DP3RObru@users.noreply.github.com>
2024-11-13 08:27:34 +0000

    Part 4

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,7 +8,8 @@ public class App
 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int odd = 0;
+        int even = 0;
         
         Statistics statistics = new Statistics();
         while(true){
@@ -16,14 +17,20 @@ public class App
             if(input == -1){
                 break;
             }
+            if (input % 2 == 0){
+                even = even + input;
+            }
+            else{
+                odd = odd + input;
+            }
             statistics.addNumber(input);
+}
+    
 
 
-
-        
-        
-    }
     System.out.println("Sum: " + statistics.sum());
+    System.out.println("Even sum: " + even);
+    System.out.println("Odd sum: " + odd);
 }}
     
     
