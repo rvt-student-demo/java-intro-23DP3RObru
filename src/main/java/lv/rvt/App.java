f1028edae3c295d4ca439021421b6ec703fe9654
23DP3RObru <166131441+23DP3RObru@users.noreply.github.com>
2024-11-15 06:37:33 +0000

    Part 4

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,13 +7,16 @@ import java.util.Random;
 public class App 
 {
     public static void main(String[] args) {
-        PaymentCard card = new PaymentCard(5);
+        PaymentCard card = new PaymentCard(10);
         System.out.println(card);
 
-        card.eatHeartily();
+        card.addMoney(15);
         System.out.println(card);
 
-        card.eatHeartily();
+        card.addMoney(10);
+        System.out.println(card);
+
+        card.addMoney(200);
         System.out.println(card);
     }
 }
--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -21,4 +21,7 @@ public class PaymentCard {
         }
         
     }
+    public void addMoney(double amount) {
+        this.balance = this.balance + amount;
+    }
 }
