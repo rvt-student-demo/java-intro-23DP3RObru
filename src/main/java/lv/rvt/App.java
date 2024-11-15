d10820e7cc31de83dc75d4a97017b8dc70d8b2d5
23DP3RObru <166131441+23DP3RObru@users.noreply.github.com>
2024-11-15 06:53:18 +0000

    Part 6 Labots

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -12,17 +12,17 @@ public class App
 
         paulsCard.eatHeartily();
         mattsCard.eatAffordably();
-        System.out.println("Paul: The card has a balnce of "+paulsCard+" euros");
-        System.out.println("Matt: The card has a balnce of "+mattsCard+" euros");
+        System.out.println("Paul: "+paulsCard);
+        System.out.println("Matt: "+mattsCard);
         paulsCard.addMoney(20);
         mattsCard.eatHeartily();
-        System.out.println("Paul: The card has a balnce of "+paulsCard+" euros");
-        System.out.println("Matt: The card has a balnce of "+mattsCard+" euros");
+        System.out.println("Paul: "+paulsCard);
+        System.out.println("Matt: "+mattsCard);
         paulsCard.eatAffordably();
         paulsCard.eatAffordably();
         mattsCard.addMoney(50);
-        System.out.println("Paul: The card has a balnce of "+paulsCard+" euros");
-        System.out.println("Matt: The card has a balnce of "+mattsCard+" euros");
+        System.out.println("Paul: "+paulsCard);
+        System.out.println("Matt: "+mattsCard);
 
     
     
