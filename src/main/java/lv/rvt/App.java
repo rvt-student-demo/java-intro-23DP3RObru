818f068cbf605d0eda02a7c6d6613d5a82f9a8de
23DP3RObru <166131441+23DP3RObru@users.noreply.github.com>
2024-11-15 06:46:27 +0000

    Part 6

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,10 +7,25 @@ import java.util.Random;
 public class App 
 {
     public static void main(String[] args) {
-        PaymentCard card = new PaymentCard(10);
-        System.out.println("Paul: " + card);
-        card.addMoney(-15);
-        System.out.println("Paul: " + card);
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
+
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
+        System.out.println("Paul: The card has a balnce of "+paulsCard+" euros");
+        System.out.println("Matt: The card has a balnce of "+mattsCard+" euros");
+        paulsCard.addMoney(20);
+        mattsCard.eatHeartily();
+        System.out.println("Paul: The card has a balnce of "+paulsCard+" euros");
+        System.out.println("Matt: The card has a balnce of "+mattsCard+" euros");
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+        mattsCard.addMoney(50);
+        System.out.println("Paul: The card has a balnce of "+paulsCard+" euros");
+        System.out.println("Matt: The card has a balnce of "+mattsCard+" euros");
+
+    
+    
     }
 }
     
